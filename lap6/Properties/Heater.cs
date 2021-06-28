fb61504c2ce09063e49c11c7bc5f01b089a03260
phamducfpt <ducpmth2009023@fpt.edu.vn>
2021-06-28 08:21:01 +0700

    first commit

---
+++ lap6/Properties/Heater.cs
@@ -0,0 +1,31 @@
+namespace lap6.Properties
+{
+    public class Heater
+    {
+        
+        public Heater(float temperature)
+        {
+            Temperature = temperature;
+        }
+
+        public float Temperature
+        {
+            get { return _Temperature; }
+            set { _Temperature = value; }
+        }
+
+        private float _Temperature;
+
+        public void OnTemperatureChanged(float newTemperature)
+        {
+            if (newTemperature < Temperature)
+            {
+                System.Console.WriteLine("Heater: On");
+            }
+            else
+            {
+                System.Console.WriteLine("Heater: Off");
+            }
+        }
+    }
+    }
